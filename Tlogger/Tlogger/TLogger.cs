9d00c1c81e543bef55a51a30875104d416b10d0e
tolga-k <t.kasoglu@gmail.com>
2015-05-09 18:00:02 +0200

    Added filename lenght check

--- Tlogger/Tlogger/TLogger.cs
+++ Tlogger/Tlogger/TLogger.cs
@@ -10,13 +10,22 @@ namespace TLoggerProject
 
     private static string _logName { get { return ConfigurationManager.AppSettings[LogName] ?? FileName; } set { ConfigurationManager.AppSettings[LogName] = value; } }
     private static string _Path { get { return ConfigurationManager.AppSettings[Path] ?? ""; } set { ConfigurationManager.AppSettings[Path] = value; } }
-    static public string GetPath { get { return ConfigurationManager.AppSettings[Path]; }}
-
+    static public string GetPath { get { return ConfigurationManager.AppSettings[Path]; } }
+    
     private const string Path = "Path", TempPath = Path, LogName = "LogName", FileName = "TLog.txt";
     #endregion
 
+    /// <summary>
+    /// With this constructor you can set the output path for the log and the logname which is going to save under.
+    /// </summary>
+    /// <param name="pathToFile"></param>
+    /// <param name="filename">Prefferably the filename with a extension</param>
     public static void InitTLogger(string pathToFile, string filename = FileName)
     {
+      if (filename.Length<0)
+      {
+        _logName = FileName;
+      }
       _logName = filename;
       _Path = pathToFile;
       InitTLogger();
@@ -24,9 +33,9 @@ namespace TLoggerProject
 
     public static void InitTLogger()
     {
-      using (StreamWriter sw = File.CreateText(ConfigurationManager.AppSettings[Path] + _logName))
+      using (StreamWriter sw = File.CreateText(_Path + _logName))
       {
-        sw.WriteLine("TLogger: Date = " + DateTime.Now.ToString());
+        sw.WriteLine("TLogger: " + DateTime.Now.ToString());
       }
     }
 
@@ -38,7 +47,7 @@ namespace TLoggerProject
 
     static public void log(object logline)
     {
-      using (StreamWriter sr = File.AppendText(ConfigurationManager.AppSettings[Path] + ConfigurationManager.AppSettings[LogName]))
+      using (StreamWriter sr = File.AppendText(_Path + _logName))
       {
         sr.WriteLine(logline.ToString());
       }
